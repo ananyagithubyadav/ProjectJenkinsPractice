786a680aa1b1e22d77d6df6ef74a5284e56f6de2
LENOVO <LENOVO@DESKTOP-2JL316I>
2026-04-30 12:40:38 +0530

    Commit for practice

---
+++ PracticeJenkins/src/test/java/demoJenkins/Demo2jenkinstest.java
@@ -0,0 +1,12 @@
+package demoJenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo2jenkinstest {
+@Test
+	public void test() {
+		Reporter.log(">>>>>>>>>>>>*********HiFrormJenkins***********<<<<<<<<<<<",true);
+	}
+
+}
