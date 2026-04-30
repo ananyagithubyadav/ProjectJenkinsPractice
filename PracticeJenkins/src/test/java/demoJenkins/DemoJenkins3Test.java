6da2db2fc1b3ee56de9e4c10bafb29c69310c585
LENOVO <LENOVO@DESKTOP-2JL316I>
2026-04-30 12:54:19 +0530

    push for 5 minutes trigger

---
+++ PracticeJenkins/src/test/java/demoJenkins/DemoJenkins3Test.java
@@ -0,0 +1,12 @@
+package demoJenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoJenkins3Test {
+@Test
+	public void test() {
+		Reporter.log(">>>>>>>>>>>>*********HiFrormJenkins***********<<<<<<<<<<<",true);
+	}
+
+}
